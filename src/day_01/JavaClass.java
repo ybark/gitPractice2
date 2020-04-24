775649948eda4cdd91beae47fc0eb294bb89b3e8
ybark <62575128+ybark@users.noreply.github.com>
2020-04-24 16:27:00 -0400

    Create JavaClass.java

---
+++ src/day_01/JavaClass.java
@@ -0,0 +1,8 @@
+ackage day_01;
+
+public class MySecondCommit {
+    public static void main(String[] args) {
+        System.out.println("I have created gitHub Acoount");
+        System.out.println("now I am sending my second code");
+    }
+}
