d6b8069586c118cfb603ae8a791152bbf44a6a94
ybark <62575128+ybark@users.noreply.github.com>
2020-04-24 16:45:46 -0400

    Create SecondCommit.java

---
+++ src/SecondCommit.java
@@ -0,0 +1,9 @@
+package day_01;
+
+public class SecondCommit {
+    public static void main(String[] args) {
+
+        // I hope everything is GOOD
+        System.out.println("from web pages");
+
+    }
