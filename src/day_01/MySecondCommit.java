088c27cf559fb4a36eac7af737e1e709151272cc
ybark <kevindoughlife.com>
2020-04-24 16:34:40 -0400

    updated

--- src/day_01/MySecondCommit.java
+++
@@ -1,8 +0,0 @@
-package day_01;
-
-public class MySecondCommit {
-    public static void main(String[] args) {
-        System.out.println("I have created gitHub Acoount");
-        System.out.println("now I am sending my second code");
-    }
-}
